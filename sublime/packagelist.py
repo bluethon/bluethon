67e317ee65d41c477833e79e446abf024d2d4bee
Bluethon <j5088794@gmail.com>
2015-01-22 22:47:25 +0800

    update sublime 150122

--- sublime/packagelist.py
+++ sublime/packagelist.py
@@ -1,1 +1,18 @@
-#
+#查看标准库帮助
+AndyPython
+#自动格式化为PEP8标准格式
+AutoPEP8
+#括号高亮
+BracketHighlighter
+#类似于git diff查看方式
+GitGutter
+#提供输入框跟随
+InputHelper
+#智能提示
+Jedi
+#插件管理
+Package Control
+#智能提示, 需要配置才能生效
+SublimeCodeIntel
+#检查代码十分符合PEP8
+SublimeLinter
