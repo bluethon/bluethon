a098e27b86c5cada5c388e5fa876921eae825069
bluethon <j5088794@gmail.com>
2016-03-07 16:59:20 +0800

    20160307 algorithm update insertion_sort

--- algorithm/insertion_sort.py
+++ algorithm/insertion_sort.py
@@ -8,16 +8,24 @@
 import numpy
 
 a = [31, 41, 59, 26, 41, 58]
+# 自动生成0-9随机排列列表
+# a = numpy.random.permutation(10)
+
+# 随机排列
+a = numpy.random.permutation(a)
+print(a)
+
+# 插入排序
 
 
-def insertion_sort(ll):
-    for x in range(1, len(ll)):
-        key = ll[x]
+def insertion_sort(ary):
+    for x in range(1, len(ary)):
+        key = ary[x]
         y = x - 1
-        while y >= 0 and ll[y] > key:
-            ll[y + 1] = ll[y]
+        while y >= 0 and ary[y] > key:
+            ary[y + 1] = ary[y]
             y -= 1
-        ll[y + 1] = key
-    return ll
+        ary[y + 1] = key
+    return ary
 
 print(insertion_sort(a))
